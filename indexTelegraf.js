c936d383d00e3406d832705126b58d2d1fa0aef4
rudnovd <rudnov.d@gmail.com>
2019-12-29 21:50:06 +0500

    feat(app): add file for startup bot with Telegraf library

---
+++ indexTelegraf.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const config = require("./config");
+const { initCollections } = require("./models/init");
+const { startBot } = require("./src/bot");
+
+mongoose.connect(config.mongoUrl, {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  startBot();
+});
+
+mongoose.connection.on("error", console.error.bind(console, "MongoDB connection error:"));
+initCollections();
