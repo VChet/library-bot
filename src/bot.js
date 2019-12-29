f3d94c3ec955eb3c16ca9f0ebe0458b6173efb58
VChet <17050347+VChet@users.noreply.github.com>
2019-12-29 23:22:50 +0500

    feat(bot init): add proxy support

--- src/bot.js
+++ src/bot.js
@@ -3,13 +3,24 @@ const config = require("../config");
 const session = require("telegraf/session");
 const Stage = require("telegraf/stage");
 const { leave } = Stage;
+const Agent = require("socks5-https-client/lib/Agent");
 
 const { scenes } = require("./scenes/index");
 
 const stage = new Stage(scenes);
 stage.command("cancel", leave());
 
-const bot = new Telegraf(config.token);
+const socksAgent = new Agent({
+  socksHost: config.socks.host,
+  socksPort: parseInt(config.socks.port),
+  socksUsername: config.socks.username,
+  socksPassword: config.socks.password,
+});
+
+const bot = new Telegraf(config.token, {
+  telegram: { agent: socksAgent }
+});
+
 bot.use(session());
 bot.use(stage.middleware());
 
