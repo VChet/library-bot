e6577e5db4556f360f19b2ddce194cbf04b0e4c5
rudnovd <rudnov.d@gmail.com>
2020-01-04 17:01:49 +0500

    feat(bot): add /books command, change take_book command

--- src/bot.js
+++ src/bot.js
@@ -2,20 +2,20 @@ const Telegraf = require("telegraf");
 const session = require("telegraf/session");
 const Stage = require("telegraf/stage");
 const Agent = require("socks5-https-client/lib/Agent");
+const { Extra } = require("telegraf");
 
 // Bot initialisation
 const config = require("../config");
 const { scenes } = require("./scenes/index");
-const { leave } = Stage;
 
 let proxy = {};
 if (config.useProxy) {
-const socksAgent = new Agent({
+  const socksAgent = new Agent({
     socksHost: config.proxy.host,
     socksPort: parseInt(config.proxy.port),
     socksUsername: config.proxy.username,
     socksPassword: config.proxy.password,
-});
+  });
   proxy = { telegram: { agent: socksAgent } };
 }
 
@@ -24,14 +24,11 @@ const bot = new Telegraf(config.token, proxy);
 exports.startBot = async function() {
   await bot.launch();
   console.log(`Bot started as ${bot.options.username}`);
-}
+};
 
 // User requests handling
 const { User } = require("../models/user");
 const stage = new Stage(scenes);
-
-// FIXME: what is this for?
-stage.command("cancel", leave());
 
 bot.use(session());
 bot.use(stage.middleware());
@@ -53,9 +50,17 @@ bot.start(ctx => {
       newUser.save();
       ctx.reply(`Привет, ${userData.username}`);
     }
-  })
+  });
 });
 
-bot.command('take_book', ctx => {
+bot.hears("/books", ctx => {
+  ctx.reply("Выберите действие:", Extra.HTML().markup(m =>
+    m.inlineKeyboard([
+      m.callbackButton("Взять книгу", "/books take")
+    ])
+  ));
+});
+
+bot.action("/books take", ctx => {
   ctx.scene.enter("searchBookScene");
 });
