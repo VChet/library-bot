24c9bdfa418ed4d4f42d3f10a6632854d24336e2
VChet <17050347+VChet@users.noreply.github.com>
2020-01-07 06:47:05 +0500

    feat(sceneHandler): show menu on button click

--- src/sceneHandler.js
+++ src/sceneHandler.js
@@ -56,21 +56,34 @@ function startSceneHandler(bot) {
   });
 
   bot.start(ctx => {
-    ctx.reply(`Снова привет, ${ctx.session.user.username}`);
+    ctx.reply(
+      `Снова привет, ${ctx.session.user.username}`,
+      Extra.HTML().markup(m => {
+        return m.inlineKeyboard([m.callbackButton("Меню", "/menu")]).resize();
+      })
+    );
   });
 
-  bot.hears("/menu", ctx => {
-    ctx.reply("Выберите действие:", Extra.HTML().markup(m =>
-      m.inlineKeyboard([
-        [
-          m.callbackButton("Поиск", "/search"),
-          m.callbackButton("Вернуть книгу", "/return")
-        ], [
-          m.callbackButton("Доступные книги", "/available"),
-          m.callbackButton("Недоступные книги", "/unavailable")
-        ]
-      ])
-    ));
+  bot.action("/menu", ctx => {
+    const hide = ctx.session.user.role !== "Admin";
+    ctx.editMessageText(
+      "Выберите действие:",
+      Extra.HTML().markup(m => {
+        const buttons = [
+          [
+            m.callbackButton("Поиск", "/search"),
+            m.callbackButton("Вернуть книгу", "/return")
+          ], [
+            m.callbackButton("Доступные книги", "/available"),
+            m.callbackButton("Недоступные книги", "/unavailable")
+          ],
+          [
+            m.callbackButton("⚠️ Пользователи", "/users", hide)
+          ]
+        ];
+        return m.inlineKeyboard(buttons);
+      })
+    );
   });
 
   bot.action("/search", ctx => {
@@ -87,17 +100,6 @@ function startSceneHandler(bot) {
 
   bot.action("/unavailable", ctx => {
     ctx.scene.enter("unavailableBooksScene");
-  });
-
-  bot.hears("/admin", isAdmin, ctx => {
-    ctx.reply("Меню администратора", Extra.HTML().markup(m =>
-      m.inlineKeyboard([
-        m.callbackButton("Пользователи", "/users")
-      ], [
-        m.callbackButton("Добавить книгу", "/book add"),
-        m.callbackButton("Редактировать", "/book edit")
-      ])
-    ));
   });
 
   bot.action("/users", isAdmin, ctx => {
