730dbf1b4190b987e7dc9e32afc18274e35d79d4
rudnovd <rudnov.d@gmail.com>
2020-01-15 18:15:49 +0500

    feat(users): add paginator component in usersScene

--- src/scenes/users.js
+++ src/scenes/users.js
@@ -3,6 +3,7 @@ const { Extra } = require("telegraf");
 
 const { declOfNum } = require("../helpers");
 const { User } = require("../../models/user");
+const { paginator } = require("../components/paginator");
 const { replyWithError } = require("../components/error");
 
 const usersScene = new Scene("usersScene");
@@ -22,7 +23,7 @@ usersScene.enter(ctx => {
     ctx.scene.session.results = users;
     ctx.editMessageText(
       `В базе ${users.length} ${declOfNum(users.length, ["пользователь", "пользователя", "пользователей"])}`,
-      listKeyboard(ctx, users)
+      paginator.keyboard(ctx, users)
     );
   });
 });
@@ -112,55 +113,7 @@ usersScene.action("menu", ctx => {
   return ctx.scene.enter("menuScene");
 });
 
-usersScene.action(/changePage (.+)/, ctx => {
-  ctx.match[1] === "next" ? ctx.scene.session.page++ : ctx.scene.session.page--;
-
-  const users = ctx.scene.session.results;
-  const currentPage = ctx.scene.session.page;
-  const firstUsersBorder = 1 + (currentPage - 1) * 10;
-  const secondUsersBorder = currentPage * 10 > users.length ? users.length : currentPage * 10;
-
-  const newMessage = `В базе ${users.length} ${declOfNum(users.length, ["пользователь", "пользователя", "пользователей"])} (показаны с ${firstUsersBorder} по ${secondUsersBorder})`;
-
-  return ctx.editMessageText(newMessage, listKeyboard(ctx, users));
-});
-
-function listKeyboard(ctx, users) {
-  const currentPage = ctx.scene.session.page;
-
-  return Extra.HTML().markup(m => {
-    const keyboard = [];
-
-    if (users.length <= 10) {
-      keyboard.push(...users.map(user => {
-        return [
-          m.callbackButton(`${user.first_name} ${user.last_name} (${user.role})`, `get ${user._id}`)
-        ];
-      }));
-    } else if (users.length > 10) {
-      keyboard.push(
-        ...users.slice((currentPage - 1) * 10, currentPage * 10).map(user => [
-          m.callbackButton(`${user.first_name} ${user.last_name} (${user.role})`, `get ${user._id}`)
-        ])
-      );
-
-      if (currentPage === 1) {
-        keyboard.push([m.callbackButton("Вперед", "changePage next")]);
-      } else if (currentPage > 1 && currentPage * 10 < users.length) {
-        keyboard.push([
-          m.callbackButton("Назад", "changePage previous"),
-          m.callbackButton("Вперед", "changePage next")
-        ]);
-      } else if (currentPage * 10 > users.length) {
-        keyboard.push([
-          m.callbackButton("Назад", "changePage previous")
-        ]);
-      }
-    }
-
-    return m.inlineKeyboard(keyboard);
-  });
-}
+usersScene.action(/changePage (.+)/, ctx => paginator.changePageAction(ctx, ctx.scene.session.results));
 
 module.exports = {
   usersScene
