3db11485b59938fd348209c938db9a6419e31892
VChet <17050347+VChet@users.noreply.github.com>
2020-01-24 01:56:45 +0500

    refactor(middleware): use defined requests

--- src/middleware.js
+++ src/middleware.js
@@ -1,17 +1,6 @@
-const { User } = require("../models/user");
+const { User } = require("../src/db/User");
 const { replyWithError } = require("./components/error");
 const config = require("../config");
-
-async function createUser(data) {
-  const newUser = new User({
-    telegram_id: data.id,
-    first_name: data.first_name,
-    last_name: data.last_name,
-    username: data.username
-  });
-  await newUser.save();
-  return newUser;
-}
 
 function updateUser(ctx) {
   if (
@@ -19,35 +8,39 @@ function updateUser(ctx) {
     ctx.session.user.last_name !== ctx.from.last_name ||
     ctx.session.user.username !== ctx.from.username
   ) {
-    User.findOneAndUpdate(
-      { telegram_id: ctx.from.id },
-      { $set:
-        {
-          first_name: ctx.from.first_name,
-          last_name: ctx.from.last_name,
-          username: ctx.from.username
-        }
-      },
-      { new: true },
-      (error) => {
-        if (error) replyWithError(ctx, error);
-      }
-    );
+    User.changeData({
+      telegram_id: ctx.from.id,
+      first_name: ctx.from.first_name,
+      last_name: ctx.from.last_name,
+      username: ctx.from.username
+    });
   }
 }
 
 async function middleware(ctx, next) {
-  // Update session data
-  let user = await User.findOne({ telegram_id: ctx.from.id }).lean();
-  if (!user) user = await createUser(ctx.from);
-  ctx.session.user = user;
-  // Update DB data
-  updateUser(ctx);
-  // Check if user is validated
-  if (config.userValidation && ctx.session.user.role === "Guest") {
-    return ctx.reply("Аккаунт ожидает подтверждения");
+  try {
+    // Update session data
+    let user = await User.isExists(ctx.from.id);
+    if (!user) {
+      const userData = {
+        telegram_id: ctx.from.id,
+        first_name: ctx.from.first_name,
+        last_name: ctx.from.last_name,
+        username: ctx.from.username
+      };
+      user = await User.addOne(userData);
+    }
+    ctx.session.user = user;
+    // Update DB data
+    updateUser(ctx);
+    // Check if user is validated
+    if (config.userValidation && ctx.session.user.role === "Guest") {
+      return ctx.reply("Аккаунт ожидает подтверждения");
+    }
+    next();
+  } catch (error) {
+    replyWithError(ctx, error);
   }
-  next();
 }
 
 module.exports = { middleware };
