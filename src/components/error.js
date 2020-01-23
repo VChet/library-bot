8fa1d815d7b41b3274b21be01c4ac99f5644e696
VChet <17050347+VChet@users.noreply.github.com>
2020-01-24 01:45:56 +0500

    fix(error): output errors to console

--- src/components/error.js
+++ src/components/error.js
@@ -1,6 +1,7 @@
 const { Extra } = require("telegraf");
 
 const replyWithError = (ctx, error) => {
+  console.log("ERROR:", error);
   return ctx.reply(
     `Ошибка! ${error}`,
     Extra.HTML().markup(m =>
